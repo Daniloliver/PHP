ec42ca67669fec1757936fa5d0e60d895f26e1f1
DANILO DE OLIVEIRA <109114316+Daniloliver@users.noreply.github.com>
2023-04-01 10:02:10 -0300

    primeiros desafios

---
+++ desafio03/conversor.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas v1.0</h1>
+        <?php 
+            $real = $_GET['num'] ?? 0;
+            $cotacão = 5.44;
+            $dolar = $real / $cotacão;
+
+            $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+            echo "<p>Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrão, $dolar, "USD") . "</strong></p>";
+
+
+        ?>
+        <button onclick="javascript:history.go(-1)"> Voltar</button>
+    </main>
+</body>
+</html>
